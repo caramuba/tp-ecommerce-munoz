090ff44d732220e2e1fb6c257586ea0bd7dc1711
CMunozBas <caramuba@gmail.com>
2025-11-05 20:34:31 -0300

    TP-Integrador correccion de estructura, creacion de carpetas frontend y backend, e instalacion de tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="bg-green rounded-lg shadow-md p-6">
+        <h1 className="text-xl font-bold text-gray-100">Hola Camo</h1>
+        <h2 className="bg-white font-bold text-blue">Toma</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
